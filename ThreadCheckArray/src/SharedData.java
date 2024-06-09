25e969df2feec5e2cd76398da2dce8112bdefb65
Israel <Israel@10.10.2.181>
2024-06-09 11:22:44 +0300

    the changes of israel to the two classes

--- ThreadCheckArray/src/SharedData.java
+++ ThreadCheckArray/src/SharedData.java
@@ -1,16 +1,29 @@
+import java.util.ArrayList;
+
 public class SharedData 
 {
-	private Integer[] array;//כאן שיניתי
-	private boolean [] winArray;
+	
+	private ArrayList<Integer> array;
+	//private Integer[] array;//כאן שיניתי
+	private boolean [] winArray;//שיניתי
 	private boolean flag;
 	private final int b;
 	
-	public SharedData(Integer[] array, int b) // כאן שיניתי
-	{
-		
+	public SharedData(ArrayList<Integer> array, int b) {
+	    this.b = 0;
 		this.array = array;
-		this.b = b;
+	    //this.num = num;
+	    this.flag = false;
+	    this.winArray = new boolean[array.size()];
 	}
+	
+	
+	//public SharedData(Integer[] array, int b) // כאן שיניתי
+	//{
+		
+		//this.array = array;
+		//this.b = b;
+	//}
 
 	public boolean[] getWinArray() 
 	{
@@ -22,10 +35,16 @@ public class SharedData
 		this.winArray = winArray;
 	}
 
-	public Integer[] getArray() // כאן שיניתי
+	public ArrayList<Integer> getArray() // כאן שיניתי
 	{
 		return array;
 	}
+	
+	
+	//public Integer[] getArray() // המקור
+	//{
+		//return array;
+	//}
 
 	public int getB() 
 	{
--- ThreadCheckArray/src/ThreadCheckArray.java
+++ ThreadCheckArray/src/ThreadCheckArray.java
@@ -1,9 +1,13 @@
+import java.util.ArrayList;
+
 public class ThreadCheckArray implements Runnable 
 {
 	private boolean flag;
 	private boolean [] winArray;
 	SharedData sd;
-	Integer[] array;//כאן שיניתי
+	
+	ArrayList<Integer> array;// השינוי החדש
+	//Integer[] array;//כאן שיניתי
 	int b;
 	
 	public ThreadCheckArray(SharedData sd) 
@@ -14,7 +18,8 @@ public class ThreadCheckArray implements Runnable
 			array = sd.getArray();
 			b = sd.getB();
 		}		
-		winArray = new boolean[array.length];
+		  winArray = new boolean[array.size()];
+		//winArray = new boolean[array.length];
 	}
 	
 	void rec(int n, int b)
@@ -26,7 +31,8 @@ public class ThreadCheckArray implements Runnable
 		}	
 		if (n == 1)
 		{
-			if(b == 0 || b == array[n-1])
+			//if(b == 0 || b == array[n-1])
+			if(b == 0 || b == array.get(n-1))
 			{
 				flag = true;
 				synchronized (sd) 
@@ -34,12 +40,14 @@ public class ThreadCheckArray implements Runnable
 					sd.setFlag(true);
 				}			
 			}
-			if (b == array[n-1])
+			if (b == array.get(n-1))
+			//if (b == array[n-1])
 				winArray[n-1] = true;
 			return;
 		}
 		
-		rec(n-1, b - array[n-1]);
+		//rec(n-1, b - array[n-1]);
+		rec(n-1, b - array.get(n-1));
 		if (flag)
 			winArray[n-1] = true;
 		synchronized (sd) 
@@ -51,13 +59,21 @@ public class ThreadCheckArray implements Runnable
 	}
 
 	public void run() {
-		if (array.length != 1)
+		//if (array.length != 1)
+		  if (array.size() != 1)
 			if (Thread.currentThread().getName().equals("thread1"))
-				rec(array.length-1, b - array[array.length - 1]);
+			{
+				  rec(array.size()-1, b - array.get([array.size() - 1]));//יש טעות סינטקס
+				//rec(array.length-1, b - array[array.length - 1]);
+			}
+
 			else 
-				rec(array.length-1, b);
-		if (array.length == 1)
-			if (b == array[0] && !flag)
+				 rec(array.size()-1, b);
+				//rec(array.length-1, b);
+		  
+		if (array.size() == 1)//שיניתי לסייז
+			//if (b == array[0] && !flag)
+			if (b == array.get(0) && !flag)
 			{
 				winArray[0] = true;
 				flag = true;
@@ -69,7 +85,7 @@ public class ThreadCheckArray implements Runnable
 		if (flag)
 		{
 			if (Thread.currentThread().getName().equals("thread1"))
-				winArray[array.length - 1] = true;
+				winArray[array.size() - 1] = true;//שיניתי לסייז
 			synchronized (sd) 
 			{
 				sd.setWinArray(winArray);
